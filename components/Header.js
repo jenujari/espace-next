d4bad6a90421713f1658cada33cd8d2cef0ffb81
Jenish Jariwala <jhon.5456@gmail.com>
2021-11-09 00:11:39 +0530

    Logo resize

--- components/Header.js
+++ components/Header.js
@@ -6,26 +6,24 @@ export default function Header() {
     <div className="head-bar">
       <div className="flex flex-row justify-between items-center h-full">
         <div className="nav-d flex-grow">
-            <Link href="/">
-          <div className="mt-8 ml-4 cursor-pointer">
-            <div style={{maxWidth:'250px'}}>
-              <Image
-                src={"/assets/images/logo.png"}
-                alt={"bee space"}
-                width="250"
-                height="50"
-              />
-              <p className="uppercase font-pop text-right text-xs opacity-70">
-                architects
-              </p>
+          <Link href="/">
+            <div className="mt-8 ml-4 cursor-pointer">
+              <div style={{ maxWidth: "300px" }}>
+                <Image
+                  src={"/assets/images/logo.png"}
+                  alt={"bee space"}
+                  width="300"
+                  height="30"
+                />
+                <p className="uppercase font-pop text-right text-xs opacity-70">
+                  architects
+                </p>
+              </div>
             </div>
-          </div>
-            </Link>
+          </Link>
         </div>
         <div className="nav-d flex-grow">
-          <div className="nav-dot">
-
-          </div>
+          <div className="nav-dot" />
         </div>
       </div>
     </div>
